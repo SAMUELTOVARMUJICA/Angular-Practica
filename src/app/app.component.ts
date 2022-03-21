8a689720347cc98ff4556b6150bbeb0d8e9fc44f
Samuel Tovar <mujica360@gmail.com>
2022-03-21 14:24:01 -0600

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FE';
+  public title: string = 'Contador App';
+  public numero: number = 10;
+  public base: number = 5;
+
+
+ acumular( valor: number ){
+   this.numero  += valor;
+ }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeroesModulo } from './Heroes/heroes.module';
+import { ContadorModulo } from './contador/contador/contador.module';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
+
+  
   ],
-  imports: [
-    BrowserModule
+  imports:[ 
+    BrowserModule,
+    HeroesModulo,
+    ContadorModulo
+
   ],
   providers: [],
   bootstrap: [AppComponent]
